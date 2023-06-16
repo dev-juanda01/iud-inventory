e3dcdd923a11fecd3de9f0dc930c7e3bc50c61db
Rafael De Jesus Molina Haeckermann <rafamolih20@gmail.com>
2023-06-15 21:11:34 -0500

    nuevos cambios routerTE

--- routes/routerTE.js
+++ routes/routerTE.js
@@ -1,14 +1,55 @@
 "use strict";
 
-const express = require("express"),
-  router = express.Router(),
-  controllerTE = require(`../controllers/ctrlTipoEquipo`);
+const { validationResult, check } = require("express-validator");
+const { idExiste } = require("../helpers/validators");
+const { validarCampos } = require("../middlewares/validar_campos");
+const { validarJWT } = require("../middlewares/validar_jwt");
+const { validarRolAdmin } = require("../middlewares/validar_rol_admin");
+const express = require("express");
+
+const router = express.Router();
+const {
+  ingresarTipoEquipo,
+  obtenerTipoEquipos,
+  obtenerTipoEquipo,
+  eliminarTipoEquipo,
+  actualizarTipoEquipo,
+} = require("../controllers/ctrlTipoEquipo");
 
 router
-  .get("/", controllerTE.obtenerTipoEquipos)
-  .get("/", controllerTE.obtenerTipoEquipo)
-  .post("/", controllerTE.ingresarTipoEquipo)
-  .put("/", controllerTE.actualizarTipoEquipo)
-  .delete("/", controllerTE.eliminarTipoEquipo);
+  .get("/", [validarJWT, validarRolAdmin], obtenerTipoEquipos)
+  .get("/", [validarJWT, validarRolAdmin], obtenerTipoEquipo)
+  .post(
+    "/",
+    [
+      // validarJWT
+      check("nombre", "Nombre obligatorio - Ingreselo").isIn([
+        "COMPUTO",
+        "MOVILES",
+      ]),
+      check("estado", "El estado es invalido - No es booleano").isBoolean(),
+      validarCampos,
+      validarJWT,
+      validarRolAdmin,
+    ],
+    ingresarTipoEquipo
+  )
+  .put(
+    "/:id",
+    [
+      check("id", "Id invalido - No es un id de mongo").isMongoId(),
+      check("id").custom(idExiste),
+      validarCampos,
+      validarJWT,
+      validarRolAdmin,
+    ],
+    actualizarTipoEquipo
+  )
+  .delete(
+    "/:id",
+    [check("id").custom(idExiste), validarCampos, validarJWT, validarRolAdmin],
+
+    eliminarTipoEquipo
+  );
 
 module.exports = router;
